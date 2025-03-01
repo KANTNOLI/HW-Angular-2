d530640c13c23788f48df2f101908850cd6c3e03
KANTNOLI <zahargyseu02@gmail.com>
2025-03-02 00:24:33 +0300

    Step 2 Ready

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  history: string[] = []
+  visual: string = "2 + 2 = 4"
 
+  valueA: number = 2
+  valueB: number = 2
+  valueC: string = "+"
+
+  setVisualExample() {
+    let result: number | string = -1
+
+    switch (this.valueC) {
+      case "+":
+        result = this.valueA + this.valueB; break;
+      case "-":
+        result = this.valueA - this.valueB; break;
+      case "/":
+        result = this.valueA / this.valueB; break;
+      case "*":
+        result = this.valueA * this.valueB; break;
+      case "**":
+        result = this.valueA ** this.valueB; break;
+      default:
+        result = 'Error 1.0'; break;
+    }
+
+    this.visual = `${this.valueA} ${this.valueC} ${this.valueA} = ${result}`
+  }
+
+  saveResult() {
+    console.log(1);
+    this.history.push(this.visual)
+    console.log(this.history);
+  }
 }
