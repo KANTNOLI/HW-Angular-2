b6c0177a4e8e5a9bb03949ef9c72f8bac9df07d9
KANTNOLI <zahargyseu02@gmail.com>
2025-03-01 22:36:12 +0300

    Starting 1 step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyComponent } from './currency/currency.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
